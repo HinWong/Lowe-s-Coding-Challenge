15d9a6c4d57d04aa87905ebd07697e7d8bdac231
Hin Wong <hinwong@Hins-MacBook-Pro.local>
2021-04-29 13:41:36 -0400

    wrote out api call

--- Lowe's/View Model/ViewModel.swift
+++ Lowe's/View Model/ViewModel.swift
@@ -7,9 +7,45 @@
 
 import Foundation
 
-func fetchWeather() {
+//https://api.openweathermap.org/data/2.5/forecast?q=London&appid=65d00499677e59496ca2f318eb68c049
+
+class WeatherViewModel {
     
-    let baseURL = URL(string: " https://api.openweathermap.org/data/2.5/forecast")
-    let apiKey = "65d00499677e59496ca2f318eb68c049"
+    static private let baseURL = URL(string: " https://api.openweathermap.org/data/2.5/forecast")
+    static private let apiKey = "65d00499677e59496ca2f318eb68c049"
     
+    static func fetchWeather(searchTerm: String, completion: @escaping (Result<[Weather], WeatherError>) -> Void) {
+        
+        //1. build url
+        guard let baseURL = baseURL else { return completion(.failure(.invalidURL)) }
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        urlComponents?.queryItems = [URLQueryItem(name: "q", value: searchTerm), URLQueryItem(name: "appid", value: apiKey)]
+        guard let finalURL = urlComponents?.url else { return completion(.failure(.invalidURL)) }
+        print(finalURL)
+        
+        //2. data task
+        URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            
+            //3. error handling
+            if let error = error {
+                print(error, error.localizedDescription)
+                return completion(.failure(.thrown(error)))
+            }
+            
+            //4. check for data
+            guard let data = data else {return completion(.failure(.noData))}
+            
+            // 5. decode data
+            do {
+                let decodedData = try JSONDecoder().decode([Weather].self, from: data)
+                completion(.success(decodedData.self))
+            } catch {
+                print(error, error.localizedDescription)
+                return completion(.failure(.thrown(error)))
+            }
+            
+        }.resume()
+    }
+
 }
+
