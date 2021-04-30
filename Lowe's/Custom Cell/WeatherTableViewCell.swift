88458c98b232dda59c712946959e30bd44ef9f0c
Hin Wong <hinwong@Hins-MacBook-Pro.local>
2021-04-30 12:19:57 -0400

    need to update label in custom cell

--- Lowe's/Custom Cell/WeatherTableViewCell.swift
+++ Lowe's/Custom Cell/WeatherTableViewCell.swift
@@ -12,6 +12,16 @@ class WeatherTableViewCell: UITableViewCell {
     @IBOutlet weak var weatherConditionsLabel: UILabel!
     @IBOutlet weak var weatherTemperatureLabel: UILabel!
     
+    func updateLabels() {
+        var viewModel: WeatherDetailsViewModel? {
+            didSet {
+                weatherTemperatureLabel.text = String(viewModel?.temperature())
+                weatherConditionsLabel.text = viewModel?.weatherDescrption()
+            }
+        }
+
+    }
+    
 
 
 }
